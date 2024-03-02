1cbb87773355208e6260f7b01c1316bc7ede1b02
Beckett Randett <beckettrandlettapps@gmail.com>
2024-03-01 19:40:26 -0500

    IT WORKSSSSSS

--- overlay.js
+++ overlay.js
@@ -22,16 +22,69 @@ function init () {
             console.log(matches)
             console.log(data)
             clearInterval(a)
+            handleOverlay(matches, data)
         }  
     }, 1000)
     console.log("sending msg")
     browser.runtime.sendMessage("hi from overlay.js")
 }
 
-function checkForLoad(e) {
-    const matches = document.querySelectorAll("div[content_resource_guid]");
-    if (matches.length > 0) {
-        console.log(matches)
-        console.log(data)
+function handleOverlay(matches, data) {
+    answerElem = document.createElement('div')
+    answerElem.style.backgroundColor = "cyan";
+    answerElem.style.position = "absolute";
+    console.log("!!!!")
+    qs = []
+    toDisplay = []
+    for (d of data) {
+        // console.log("aaa")
+        if ('payload' in d) {
+            if ('questions' in d.payload) {
+                for (q of d.payload.questions) {
+                    qs.push(q)
+                }
+            }
+        }
     }
+    for (q of qs) {
+        if ('text' in q) {
+            txt = ''
+            ans = []
+            for (t of q.text) {
+                if ('text' in t) {
+                    txt += t.text
+                }
+                
+                // console.log(txt)
+                if ('answers' in q) {ans=q.answers}
+                if ('choices' in q) {
+                    for (c of q.choices) {
+                        if (c.correct && 'label' in c) {
+                            ltxt = ''
+                            for (l of c.label) {
+                                if ('text' in l) {
+                                    ltxt += l.text
+                                }
+                            }
+                            ans.push(ltxt)
+                        }
+                    }
+                }
+            }
+            toDisplay.push({txt, ans})
+            console.log({txt, ans})
+        }
+    }
+
+    for (z of toDisplay) {
+        qElem = document.createElement('div')
+        qElem.innerText = z.txt
+        aElem = document.createElement('div')
+        aElem.innerText = z.ans
+        qElem.style.color = "darkgrey"
+        answerElem.appendChild(qElem)
+        answerElem.appendChild(aElem)
+    }
+    answerElem.style.zIndex = "1000000"
+    document.body.appendChild(answerElem)
 }
